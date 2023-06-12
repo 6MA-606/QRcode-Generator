a23c2d4dcf9402a6e7f7277ed368a43663de2154
6MA-606 <zyxma.6666@hotmail.com>
2023-06-12 21:24:32 +0700

    2.1.0 alpha 3.0 - Image Size

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ import Head from "next/head";
 import QRCodeComponent from "@/components/QRCodeComponent";
 
 export default function Home() {
-  const version = "2.1.0 alpha 2.0";
+  const version = "2.1.0 alpha 3.0";
   const [isDarkmode, setIsDarkmode] = useState(false);
   const [text, setText] = useState("");
   const [color, setColor] = useState("#000000");
